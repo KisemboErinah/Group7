b2d93304ac1e9213757b54e023289e80d15bb5db
Mahmood-47 <nyendemahmood386@gmail.com>
2018-06-12 12:29:31 +0300

    Maggie's modification

--- demos.R
+++ demos.R
@@ -1,9 +1,9 @@
 chess <- read.csv("players.csv",TRUE,",")
-chess
+
 plot(chess$B.YEAR,chess$GAMES ,xlab = "year of birth", ylab="number of games", main="relationship between years and number of games played")
 lm(chess$GAMES~chess$B.YEAR)
 abline(lm(chess$GAMES~chess$B.YEAR))
-pre<-data.frame(chess$B.YEAR=1975)
+pre<-data.frame(chess$B.YEAR=1974)
 var<-lm(chess$GAMES~chess$B.YEAR)
 predict(var,pre)
 
