726db7f4ebe05b09bf8b8d2e6d2555d085923bb1
KisemboErinah <erinahkisembo@gmail.com>
2018-06-12 17:36:29 +0300

    simple change

--- demos.R
+++ demos.R
@@ -1,9 +1,20 @@
-chess <- read.csv("players.csv",TRUE,",")
-chess
+chess <- read.csv("players.csv",TRUE,",") #obtain the dataset
+
+#plot number of games played against year of birth
 plot(chess$B.YEAR,chess$GAMES ,xlab = "year of birth", ylab="number of games", main="relationship between years and number of games played")
-lm(chess$GAMES~chess$B.YEAR)
-abline(lm(chess$GAMES~chess$B.YEAR))
-pre<-data.frame(chess$B.YEAR=1975)
-var<-lm(chess$GAMES~chess$B.YEAR)
-predict(var,pre)
+
+#find the relationship between the number of games played and the year of birth
+relationship<- lm(chess$GAMES~chess$B.YEAR)
+summary(relationship)
+coeff <- coefficients(relationship)
+print(coeff)
+
+abline(lm(chess$GAMES~chess$B.YEAR))#draw the line of regression
+pre <- data.frame(chess$B.YEAR <- 1999)
+
+
+#predicting the number of games played given the year of birth
+var<-predict(relationship,pre)
+print(var)
+
 
