b10e4a1daf6ac150e8105b8f769d83210dc13d7e
KisemboErinah <erinahkisembo@gmail.com>
2018-06-14 13:09:34 +0300

    new changes

--- demos.R
+++ demos.R
@@ -1,50 +1,11 @@
+data("trees") #load data set trees
+View(trees) #have a look at the data set
+plot(Volume ~ Girth, data = trees, log = "xy") #plot scatter graph of Volume against Girth
+TreesModel <- lm(Volume ~ Girth, data = trees) #Evaluate the correlation between Girth and volume
+print(TreesModel)
+abline(TreesModel)
+summary(TreesModel)
 
-chess <- read.csv("players.csv",TRUE,",") #obtain the dataset
-
-#plot number of games played against year of birth
-plot(chess$B.YEAR,chess$GAMES ,xlab = "year of birth", ylab="number of games", main="relationship between years and number of games played")
-
-#find the relationship between the number of games played and the year of birth
-relationship<- lm(chess$GAMES~chess$B.YEAR)
-summary(relationship)
-coeff <- coefficients(relationship)
-print(coeff)
-
-abline(lm(chess$GAMES~chess$B.YEAR))#draw the line of regression
-pre <- data.frame(chess$B.YEAR <- 1999)
-
-
-#predict the number of games played given the year of birth
-var<-predict(relationship,pre)
-print(var)
-
-
-chess <- read.csv("players.csv",TRUE,",") #obtain the dataset
-
-#plot number of games played against year of birth
-plot(chess$B.YEAR,chess$GAMES ,xlab = "year of birth", ylab="number of games", main="relationship between years and number of games played")
-
-#find the relationship between the number of games played and the year of birth
-relationship<- lm(chess$GAMES~chess$B.YEAR)
-summary(relationship)
-coeff <- coefficients(relationship)
-print(coeff)
-
-abline(lm(chess$GAMES~chess$B.YEAR))#draw the line of regression
-pre <- data.frame(chess$B.YEAR <- 1998)
-
-
-#predict the number of games played given the year of birth
-var<-predict(relationship,pre)
-print(var)
-
-chess <- read.csv("players.csv",TRUE,",")
-
-plot(chess$B.YEAR,chess$GAMES ,xlab = "year of birth", ylab="number of games", main="relationship between years and number of games played")
-lm(chess$GAMES~chess$B.YEAR)
-abline(lm(chess$GAMES~chess$B.YEAR))
-pre<-data.frame(chess$B.YEAR=1974)
-var<-lm(chess$GAMES~chess$B.YEAR)
-predict(var,pre)
-
-
+a <- data.frame(Girth = 15) #make a data frame of the predict function
+result <- predict(TreesModel,a) #predict function
+print(result)
