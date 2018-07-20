e3c6377f980d375dc0acc1984ccd4caa4590cf81
Mahmood-47 <40106096+Mahmood-47@users.noreply.github.com>
2018-07-20 12:27:27 +0000

    Part of the  system

--- ui.r
+++ ui.r
@@ -7,7 +7,8 @@ library(reshape2)
 #library(plotly)
 library(leaflet)
 library(htmltools)
-
+library(scales)
+library(dplyr)
 shinyUI(
   dashboardPage(skin = ("red"),
                 dashboardHeader(title = ("STACK OVERFLOW")),
@@ -42,7 +43,7 @@ shinyUI(
                     # First tab content................................dashboard items begins from here....................................................
                     tabItem(tabName = "home",
                             fluidRow(
-                              box(width=100,title="The Stack Overflow Developers' Survey Analysis System.",background="red",solidHeader=F,status="success",uiOutput("picto"),tableOutput("out2")),
+                              box(width=100,title="The Stack Overflow Developers' Survey Analysis System.",background="red",solidHeader=F,status="success",uiOutput("picto"),tableOutput("out2"),tableOutput("out")),
                               
                               
                               box(width=100,solidHeader = T,status = "success",
@@ -73,18 +74,23 @@ shinyUI(
                     
                     tabItem(tabName = "gets",
                             fluidRow(
-                              box(width=100,title="A bar graph showing the formal education levels of the survey participants", plotOutput("first") ))
+                              box(width=100,title="A bar graph showing the formal education levels of the survey participants" ))
                             
                     ),
                     
                     tabItem(tabName = "plots",
-                            fluidRow(
-                              box(uiOutput("tinx")),
-                              box(
-                                title = "Visualizations",
-                                plotOutput("this")  
-                              ))
-                            
+                             fluidRow(
+                               box(uiOutput("tinx")),
+                               box(uiOutput("tin")),
+                              
+                               box(
+                                 title = "Bar chart",
+                                 plotOutput("first")  
+                               ),box(textOutput("tek")),box(
+                                 title = "Visualizations",
+                                 plotOutput("this")  
+                               ))
+                             
                     )
                   )
                   )
